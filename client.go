379d5ee7c776ef55f8662a3ae58cd37d73aef3c9
Anton Rydén <34473963+anton-ryden@users.noreply.github.com>
2022-11-28 10:56:08 +0100

    init

---
+++ client.go
@@ -0,0 +1,8 @@
+package Distributed_storage_on_chord
+
+import "flag"
+
+func main() {
+	ipPtr := flag.String("a", "", " The IP address that the Chord client will bind to, as well as advertise to other nodes. Represented as an ASCII string (e.g., 128.8.126.63)")
+
+}
